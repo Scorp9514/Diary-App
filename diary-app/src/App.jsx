cb99cf503c2fad0d17763f1263a0d83c8f371841
Scorp9514 <ertyu@gmail.com>
2025-11-05 20:07:21 +0800

    testing main.jsx

--- diary-app/src/App.jsx
+++ diary-app/src/App.jsx
@@ -1,23 +1,30 @@
 import "./css/app.css"
 
-import {Routes,Route,UNSAFE_DataRouterStateContext} from 'react-router-dom'
+import {Routes,Route,UNSAFE_DataRouterStateContext, BrowserRouter} from 'react-router-dom'
 import {useState} from 'react';
 
 import NavBar from "./components/NavBar"
 import Calendars from "./components/calendars"
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
+import Favorites from "./pages/Favorites.jsx";
+
 
 function App() {
 
   return (
     <>
-
       <div className="background">
         <NavBar/>
+        <main>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/Favorites" element={<Favorites/>}/>
         </Routes>
+        
+        </main>
+        
       </div>
+      
     </>
   )
 }
